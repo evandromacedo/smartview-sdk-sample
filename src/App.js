8fbd5eec2b985ef53c6efe1de2c9fb24c0be9379
Evandro Macedo <evandro.macedo@g.globo>
2020-11-24 16:00:36 -0300

    Instala vendor do smart view sdk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import scriptjs from 'scriptjs';
+import { MSF_2_3_4_MIN_JS } from './vendor';
+
+const LOGGER_LENGTH = 20;
 
 function App() {
+  const [logger, updateLogger] = useState([]);
+
+  const logOnScreen = (newLog) => {
+    updateLogger((prevLog) => {
+      if (prevLog.length >= LOGGER_LENGTH) {
+        return [...prevLog.slice(1, LOGGER_LENGTH), newLog];
+      }
+
+      return [...prevLog, newLog];
+    });
+  };
+
+  useEffect(() => {
+    scriptjs(MSF_2_3_4_MIN_JS, () => {
+      logOnScreen("Carregou script do SmartView SDK!");
+
+      window.msf.search((err, services) => {
+        logOnScreen("Entrou no msf.search!");
+        if (err) logOnScreen(`Erro ao conectar ao canal: ${err}`);
+        logOnScreen(`Services: ${services.length}`);
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Poc Wifi!</h1>
+      {logger.map((log, index) => (
+        <p key={index}>{log}</p>
+      ))}
     </div>
   );
 }
